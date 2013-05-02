412e8badc88078b1c792459129ee5e032cefe6bd
guymoyo <guymoyo@yahoo.fr>
2013-05-02 10:05:41 +0100

    .

--- adpharma/src/main/java/org/adorsys/adpharma/beans/ChangeDatePrice.java
+++ adpharma/src/main/java/org/adorsys/adpharma/beans/ChangeDatePrice.java
@@ -1,9 +1,13 @@
 package org.adorsys.adpharma.beans;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import org.springframework.format.annotation.DateTimeFormat;
+
 
 public class ChangeDatePrice {
 	
@@ -11,9 +15,9 @@ public class ChangeDatePrice {
 	
 	private String designation;
 	
-	private Bigdecimal prixActuel;
+	private BigDecimal prixActuel;
 	
-	private Bigdecimal nouveauPrix;
+	private BigDecimal nouveauPrix;
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	@DateTimeFormat(pattern = "dd-MM-yyyy")
@@ -26,5 +30,73 @@ public class ChangeDatePrice {
 	private boolean appliqueLeNouveauPrixATousLesCipmDeMemeCip =true;
 	
 	private boolean appliqueLaNouvelDateATousLesCipmDeMemeCip =true;
+
+	public String getCipm() {
+		return cipm;
+	}
+
+	public void setCipm(String cipm) {
+		this.cipm = cipm;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public BigDecimal getPrixActuel() {
+		return prixActuel;
+	}
+
+	public void setPrixActuel(BigDecimal prixActuel) {
+		this.prixActuel = prixActuel;
+	}
+
+	public BigDecimal getNouveauPrix() {
+		return nouveauPrix;
+	}
+
+	public void setNouveauPrix(BigDecimal nouveauPrix) {
+		this.nouveauPrix = nouveauPrix;
+	}
+
+	public Date getDatePeremptionActuel() {
+		return datePeremptionActuel;
+	}
+
+	public void setDatePeremptionActuel(Date datePeremptionActuel) {
+		this.datePeremptionActuel = datePeremptionActuel;
+	}
+
+	public Date getNouvelDatePeremption() {
+		return nouvelDatePeremption;
+	}
+
+	public void setNouvelDatePeremption(Date nouvelDatePeremption) {
+		this.nouvelDatePeremption = nouvelDatePeremption;
+	}
+
+	public boolean isAppliqueLeNouveauPrixATousLesCipmDeMemeCip() {
+		return appliqueLeNouveauPrixATousLesCipmDeMemeCip;
+	}
+
+	public void setAppliqueLeNouveauPrixATousLesCipmDeMemeCip(
+			boolean appliqueLeNouveauPrixATousLesCipmDeMemeCip) {
+		this.appliqueLeNouveauPrixATousLesCipmDeMemeCip = appliqueLeNouveauPrixATousLesCipmDeMemeCip;
+	}
+
+	public boolean isAppliqueLaNouvelDateATousLesCipmDeMemeCip() {
+		return appliqueLaNouvelDateATousLesCipmDeMemeCip;
+	}
+
+	public void setAppliqueLaNouvelDateATousLesCipmDeMemeCip(
+			boolean appliqueLaNouvelDateATousLesCipmDeMemeCip) {
+		this.appliqueLaNouvelDateATousLesCipmDeMemeCip = appliqueLaNouvelDateATousLesCipmDeMemeCip;
+	}
+	
+	
 
 }
